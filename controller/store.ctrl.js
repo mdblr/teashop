baea90ab11c8a9ccf405b8ceb93696ef9366402f
Max Dobler <mdblr@outlook.com>
2016-05-18 10:20:09 -0700

    deprecate add and remove fns for alterChange fn

--- controller/store.ctrl.js
+++ controller/store.ctrl.js
@@ -1,25 +1,29 @@
 (function() {
-  angular
-    .module('teaTime')
-    .controller('Store', Store)
+    angular
+        .module('teaTime')
+        .controller('Store', Store)
 
     function Store(StoreFront, Shop) {
 
-      var vm, gather;
+        var vm, gather;
 
-      gather = StoreFront.getItems();
-      vm = this;
-      vm.products = [];
+        vm = this;
+        vm.products = [];
+        vm.val = 1;
+        gather = StoreFront.getItems();
 
-      gather.then(function(tea) {
-          vm.products = tea;
-          return vm.shelves;
-      });
+        gather.then(function(tea) {
+            vm.products = tea.products;
+            vm.filter = tea.categories;
+        });
 
-      vm.select = Shop.add; /*(id)*/
-      vm.unselect =  Shop.remove; /*(id, 1)*/
-      vm.remove = Shop.remove; /*(id)*/
-
+        vm.cart = Shop.cart()
+        vm.alterCart = Shop.change;
+        vm.category = 'all';
+        vm.itemPrice = 'none';
+        // vm.select = Shop.add; /*(id)*/
+        // vm.unselect =  Shop.remove; /*(id)*/
+        // vm.delete =  Shop.remove; /*(id, 0)*/
     }
 
 })();
