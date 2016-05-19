6a1e4459d00dbe9cd6bfc11a603acd914d9656fa
Max Dobler <mdblr@outlook.com>
2016-05-19 00:08:45 -0700

    add property length to cart object

--- services/cart.service.js
+++ services/cart.service.js
@@ -9,11 +9,13 @@
 
       items = {};
       cart = {};
-
+      cart.length = 0;
+      
       items.add = function(tea, val) {
         if (!cart[tea._id]) {
           cart[tea._id] = tea;
           cart[tea._id].qty = parseInt(val);
+          cart.length++;
           return;
         }
         cart[tea._id].qty += parseInt(val);
@@ -26,6 +28,7 @@
 
       items.remove = function(tea) {
         delete cart[tea._id];
+        cart.length--;
       }
 
       items.cart = function() {
@@ -34,9 +37,12 @@
 
       items.total = function () {
         var orderTotal = 0;
+        var cartLength = 0;
         for (item in cart) {
           orderTotal += cart[item].qty * cart[item].price;
+          cartLength += 1 ;
         }
+        console.log(cartLength);
         return orderTotal;
 
       }
