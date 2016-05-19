870ab7a07136ad42009faec3adc99fa2ebad4dcd
Max Dobler <mdblr@outlook.com>
2016-05-18 22:03:37 -0700

    add dir and link to edit cart and manipulate DOM

--- directives/EditCart.directive.js
+++ directives/EditCart.directive.js
@@ -1,15 +1,32 @@
 (function() {
   angular
     .module('teaTime')
-    .directive('deEditCart', deEditCart)
+    .directive('edit', edit)
 
-    function deEditCart() {
+    function edit() {
       return {
-        scope: {
-          ctrl: '=',
-          function: '&'
-        },
-        templateUrl: 'partials/EditCart.html'
+        link: link,
+        controller: edit,
+        controllerAs: 'edit'
+      }
+
+      function edit(Shop) {
+        var vm = this;
+        vm.keepValue = true;
+        vm.orderTotal = Shop.total();
+      }
+
+      function link (scope, element, attrs, edit) {
+        element.on('click', function() {
+          if (!edit.keepValue) {
+            element.removeClass("btn-info").addClass("btn-warning");
+            element.text('Save')
+          } else {
+            element.removeClass("btn-warning").addClass("btn-info");
+            element.text('Edit')
+          }
+
+        })
       }
     }
 
